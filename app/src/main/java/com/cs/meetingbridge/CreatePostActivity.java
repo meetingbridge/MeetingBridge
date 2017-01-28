1012fe157b9da4c3db88dcbbb6bb36c232101416
Haseeb <meetingbridge13@gmail.com>
2017-01-28 16:11:41 +0500

    Starting Post Activity

---
+++ app/src/main/java/com/cs/meetingbridge/CreatePostActivity.java
@@ -0,0 +1,48 @@
+package com.cs.meetingbridge;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class CreatePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button setTimeButton = (Button) findViewById(R.id.setTime);
+        Button setDateButton = (Button) findViewById(R.id.setDate);
+        setTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "content_create_post");
+                //(getFragmentManager(),"post_layout");
+            }
+        });
+        setDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "content_post_create");
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/cs/meetingbridge/DiscussionFragment.java
+++ app/src/main/java/com/cs/meetingbridge/DiscussionFragment.java
@@ -1,10 +1,12 @@
 package com.cs.meetingbridge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -41,6 +43,7 @@ public class DiscussionFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_discussion, container, false);
         final TextView textView = (TextView) rootView.findViewById(R.id.textView);
+        DiscussionFragment context = this;
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             final String id = bundle.getString("id");
@@ -60,6 +63,13 @@ public class DiscussionFragment extends Fragment {
             });
 
         }
+        Button createPost = (Button) rootView.findViewById(R.id.createPostButton);
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), CreatePostActivity.class));
+            }
+        });
         return rootView;
     }
 
